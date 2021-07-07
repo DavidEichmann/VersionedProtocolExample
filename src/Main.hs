6ebe4227b2a4a22d392e2dbeef10edaea1c11a85
David Eichmann <EichmannD@gmail.com>
2021-07-07 13:02:00 +0100

    Backwards compatible client

--- src/Main.hs
+++ src/Main.hs
@@ -29,19 +29,50 @@ main = do
   args <- getArgs
   case args of
     -- Now we have a backwards compatible server :-)
-    ['s' : _] ->
-      runServer
-        V2.protocolVersion
-        ( \(ProtocolVersion v) -> case v of
-            -- use V1 codec (decoder) and downgrad v2 server to v1 server
-            1 -> SomePeer V1.codec (V2.downgradeServerV2ToV1 V2.server)
-            -- use V2 codec and server
-            2 -> SomePeer V2.codec V2.server
-            _ -> error $ "Unknown version: " ++ show v
-        )
+    ['s' : _, versionStr] -> case read versionStr :: Int of
+      -- Act as an old server (in practice this is just simulating an old
+      -- release of the server)
+      1 ->
+        runServer
+          V1.protocolVersion
+          ( \(ProtocolVersion v) -> case v of
+              -- use V1 codec (decoder) and downgrad v2 server to v1 server
+              1 -> SomePeer V1.codec (V2.downgradeServerV2ToV1 V2.server)
+              _ -> error $ "Unknown version: " ++ show v
+          )
+      -- In practice we'll only have this case i.e. the latest backwards
+      -- compatible version of the server
+      2 ->
+        runServer
+          V2.protocolVersion
+          ( \(ProtocolVersion v) -> case v of
+              -- use V1 codec (decoder) and downgrad v2 server to v1 server
+              1 -> SomePeer V1.codec (V2.downgradeServerV2ToV1 V2.server)
+              -- use V2 codec and server
+              2 -> SomePeer V2.codec V2.server
+              _ -> error $ "Unknown version: " ++ show v
+          )
+      _ -> error $ "Unknown version: " ++ versionStr
     ['c' : _, versionStr] -> case read versionStr :: Int of
-      1 -> runClient V1.protocolVersion (\(ProtocolVersion _) -> SomePeer V1.codec V1.client)
-      2 -> runClient V2.protocolVersion (\(ProtocolVersion _) -> SomePeer V2.codec V2.client)
+      -- Act as an old client (in practice this is just simulating an old
+      -- release of the client)
+      1 ->
+        runClient
+          V1.protocolVersion
+          ( \(ProtocolVersion v) -> case v of
+              1 -> SomePeer V1.codec V1.client
+              _ -> error $ "Unknown version: " ++ show v
+          )
+      -- In practice we'll only have this case i.e. the latest version of the
+      -- client
+      2 ->
+        runClient
+          V2.protocolVersion
+          ( \(ProtocolVersion v) -> case v of
+              1 -> SomePeer V1.codec (V2.downgradeClientV2ToV1 V2.client)
+              2 -> SomePeer V2.codec V2.client
+              _ -> error $ "Unknown version: " ++ show v
+          )
       _ -> error $ "Unknown version: " ++ versionStr
     _ ->
       putStrLn $
--- src/Protocol/V2Compat.hs
+++ src/Protocol/V2Compat.hs
@@ -15,6 +15,7 @@
 module Protocol.V2Compat where
 
 import Core
+import Data.Text
 import qualified Protocol.V1 as V1
 import qualified Protocol.V2 as V2
 
@@ -60,3 +61,51 @@ downgradeServerV2ToV1 server2Top = goIdle server2Top
             (ServerAgency V1.TokPinged)
             V1.Pong
             (goIdle server2')
+
+downgradeClientV2ToV1 ::
+  forall m a.
+  Monad m =>
+  Peer V2.MyProtocol AsClient V2.StIdle m a ->
+  Peer V1.MyProtocol AsClient V1.StIdle m a
+downgradeClientV2ToV1 server2Top = goIdle server2Top
+  where
+    goIdle ::
+      Peer V2.MyProtocol AsClient V2.StIdle m a ->
+      Peer V1.MyProtocol AsClient V1.StIdle m a
+    goIdle client2 = case client2 of
+      Effect eff -> Effect (goIdle <$> eff)
+      Yield (ClientAgency tok) msg client2' -> case msg of
+        V2.Ping -> Yield (ClientAgency V1.TokIdle) V1.Ping (goPinged client2')
+        V2.Stop -> Yield (ClientAgency V1.TokIdle) V1.Stop (goStopped client2')
+        V2.Echo txt -> goEchoed txt client2'
+
+    goPinged ::
+      Peer V2.MyProtocol AsClient V2.StPinged m a ->
+      Peer V1.MyProtocol AsClient V1.StPinged m a
+    goPinged client2 = case client2 of
+      Effect eff -> Effect (goPinged <$> eff)
+      Await (ServerAgency V2.TokPinged) client2' ->
+        Await
+          (ServerAgency V1.TokPinged)
+          ( \msg1 -> case msg1 of
+              V1.Pong -> goIdle (client2' V2.Pong)
+          )
+
+    goStopped ::
+      Peer V2.MyProtocol AsClient V2.StDone m a ->
+      Peer V1.MyProtocol AsClient V1.StDone m a
+    goStopped client2 = case client2 of
+      Effect eff -> Effect (goStopped <$> eff)
+      Done V2.TokDone a -> Done V1.TokDone a
+
+    -- Here we use domain knowledge of how the server should act in order to
+    -- make the client backwards compatible. This will only work in simple cases
+    -- I'm afraid. More commonly we might want to write a client that can check
+    -- the version and provide different behaviors accordingly.
+    goEchoed ::
+      Text ->
+      Peer V2.MyProtocol AsClient V2.StEchoed m a ->
+      Peer V1.MyProtocol AsClient V1.StIdle m a
+    goEchoed txt client2 = case client2 of
+      Effect eff -> Effect (goEchoed txt <$> eff)
+      Await (ServerAgency V2.TokEchoed) client2' -> goIdle (client2' (V2.EchoResp txt))
