79643a13e1616a4beb69f8bd2b509efc17fb228a
David Eichmann <EichmannD@gmail.com>
2021-07-29 16:48:07 +0100

    Handshake uses version ranges and client upgrades to negotiated version

--- src/Core/Run.hs
+++ src/Core/Run.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
@@ -11,7 +12,13 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeInType #-}
 
-module Core.Run where
+module Core.Run
+  ( runServer,
+    runClient,
+    ProtocolVersion (..),
+    SomePeer (..),
+  )
+where
 
 import qualified Codec.Serialise as S
 import Control.Monad.ST
@@ -21,7 +28,7 @@ import Control.Monad.ST
 import Core
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, fromMaybe)
 import Network.Simple.TCP
 
 port :: ServiceName
@@ -36,44 +43,72 @@ data SomePeer r a
 
 newtype ProtocolVersion = ProtocolVersion Int
 
+maxCommonVersion ::
+  Int -> -- Peer A's min supported version
+  Int -> -- Peer A's max supported version
+  Int -> -- Peer B's min supported version
+  Int -> -- Peer B's max supported version
+  Maybe Int
+maxCommonVersion minA maxA minB maxB =
+  if lo <= hi
+    then Just hi
+    else Nothing
+  where
+    lo = max minA minB
+    hi = min maxA maxB
+
+handShake ::
+  -- | Min protocol version supported
+  ProtocolVersion ->
+  -- | Max protocol version supported
+  ProtocolVersion ->
+  Socket ->
+  -- | Negotiated version
+  IO ProtocolVersion
+handShake (ProtocolVersion minA) (ProtocolVersion maxA) socket = do
+  -- Hand Shake
+  send socket (BS.toStrict (S.serialise @Int minA))
+  send socket (BS.toStrict (S.serialise @Int maxA))
+  minB <- S.deserialise @Int . BS.fromStrict . fromJust <$> recv socket 1
+  maxB <- S.deserialise @Int . BS.fromStrict . fromJust <$> recv socket 1
+
+  putStrLn $ "My      supported version range: [" ++ show minA ++ ".." ++ show maxA ++ "]"
+  putStrLn $ "Other's supported version range: [" ++ show minB ++ ".." ++ show maxB ++ "]"
+  let negotiatedVersion = fromMaybe (error "FAILED to find a common version") (maxCommonVersion minA maxA minB maxB)
+  putStrLn $ "negotiated version             : " ++ show negotiatedVersion
+  return (ProtocolVersion negotiatedVersion)
+
 runClient ::
+  -- | Min protocol version supported
+  ProtocolVersion ->
   -- | Max protocol version supported
   ProtocolVersion ->
   (ProtocolVersion -> SomePeer AsClient a) ->
   IO a
-runClient (ProtocolVersion maxVersion) mkPeer =
+runClient minVersion maxVersion mkPeer =
   connect "127.0.0.1" port $ \(connectionSocket, remoteAddr) -> do
     putStrLn $ "Connection established to " ++ show remoteAddr
-
-    -- Hand Shake
-    send connectionSocket (BS.toStrict (S.serialise @Int maxVersion))
-    v <- S.deserialise @Int . BS.fromStrict . fromJust <$> recv connectionSocket 8
-
-    putStrLn $ "Max supported version: " ++ show maxVersion
-    let negotiatedVersion = min maxVersion v
-    putStrLn $ "negotiated version   : " ++ show negotiatedVersion
-
-    case mkPeer (ProtocolVersion negotiatedVersion) of
-      SomePeer msgDecoder peer -> runPeer msgDecoder BS.empty connectionSocket peer
+    negotiatedVersion <- handShake minVersion maxVersion connectionSocket
+    case mkPeer negotiatedVersion of
+      SomePeer msgDecoder peer ->
+        runPeer
+          msgDecoder
+          BS.empty
+          connectionSocket
+          peer
 
 runServer ::
+  -- | Min protocol version supported
+  ProtocolVersion ->
   -- | Max protocol version supported
   ProtocolVersion ->
   (ProtocolVersion -> SomePeer AsServer ()) ->
   IO a
-runServer (ProtocolVersion maxVersion) mkPeer =
+runServer minVersion maxVersion mkPeer =
   serve (Host "127.0.0.1") port $ \(connectionSocket, remoteAddr) -> do
     putStrLn $ "Connection established to " ++ show remoteAddr
-
-    -- Hand Shake
-    send connectionSocket (BS.toStrict (S.serialise @Int maxVersion))
-    v <- S.deserialise @Int . BS.fromStrict . fromJust <$> recv connectionSocket 8
-
-    putStrLn $ "Max supported version: " ++ show maxVersion
-    let negotiatedVersion = min maxVersion v
-    putStrLn $ "negotiated version   : " ++ show negotiatedVersion
-
-    case mkPeer (ProtocolVersion negotiatedVersion) of
+    negotiatedVersion <- handShake minVersion maxVersion connectionSocket
+    case mkPeer negotiatedVersion of
       SomePeer msgDecoder peer -> runPeer msgDecoder BS.empty connectionSocket peer
 
 runPeer ::
--- src/Main.hs
+++ src/Main.hs
@@ -31,21 +31,23 @@ main = do
       -- release of the server)
       1 ->
         runServer
-          V1.protocolVersion
+          (ProtocolVersion 1)
+          (ProtocolVersion 1)
           ( \(ProtocolVersion v) -> case v of
               -- use V1 codec (decoder) and downgrad v2 server to v1 server
               1 -> SomePeer V1.codec V1.server
-              _ -> error $ "Unknown version: " ++ show v
+              _ -> error $ "Unsupported version: " ++ show v
           )
       2 ->
         runServer
-          V2.protocolVersion
+          (ProtocolVersion 1)
+          (ProtocolVersion 2)
           ( \(ProtocolVersion v) -> case v of
               -- use V1 codec (decoder) and downgrad v2 server to v1 server
               1 -> SomePeer V1.codec server1
               -- use V2 codec and server
               2 -> SomePeer V2.codec server2
-              _ -> error $ "Unknown version: " ++ show v
+              _ -> error $ "Unsupported version: " ++ show v
           )
         where
           server1 = V2.downgradeServerV2ToV1 server2
@@ -54,52 +56,65 @@ main = do
       -- compatible version of the server
       3 ->
         runServer
-          V3.protocolVersion
+          (ProtocolVersion 1)
+          (ProtocolVersion 3)
           ( \(ProtocolVersion v) -> case v of
               -- use V1 codec (decoder) and downgrad v2 server to v1 server
               1 -> SomePeer V1.codec server1
               -- use V2 codec and server
               2 -> SomePeer V2.codec server2
               3 -> SomePeer V3.codec server3
-              _ -> error $ "Unknown version: " ++ show v
+              _ -> error $ "Unsupported version: " ++ show v
           )
         where
           server1 = V2.downgradeServerV2ToV1 server2
           server2 = V3.downgradeServerV3ToV2 server3
           server3 = V3.server
-      _ -> error $ "Unknown version: " ++ versionStr
-    ['c' : _, versionStr] -> case read versionStr :: Int of
+      _ -> error $ "Unsupported version: " ++ versionStr
+    ['c' : _, versionStr, maxVStr] | let maxV = read maxVStr -> case read versionStr :: Int of
       -- Act as an old client (in practice this is just simulating an old
       -- release of the client)
       1 ->
         runClient
-          V1.protocolVersion
+          (ProtocolVersion 1)
+          (ProtocolVersion (min maxV 3))
           ( \(ProtocolVersion v) -> case v of
-              1 -> SomePeer V1.codec V1.client
-              _ -> error $ "Unknown version: " ++ show v
+              1 -> SomePeer V1.codec client1
+              2 -> SomePeer V2.codec client2
+              3 -> SomePeer V3.codec client3
+              _ -> error $ "Unsupported version: " ++ show v
           )
+        where
+          client1 = V1.client
+          client2 = V2.upgradeClientV1ToV2 client1
+          client3 = V3.upgradeClientV2ToV3 client2
       2 ->
         runClient
-          V2.protocolVersion
+          (ProtocolVersion 2)
+          (ProtocolVersion (min maxV 3))
           ( \(ProtocolVersion v) -> case v of
-              1 -> SomePeer V1.codec (V2.downgradeClientV2ToV1 V2.client)
-              2 -> SomePeer V2.codec V2.client
-              _ -> error $ "Unknown version: " ++ show v
+              2 -> SomePeer V2.codec client2
+              3 -> SomePeer V3.codec client3
+              _ -> error $ "Unsupported version: " ++ show v
           )
+        where
+          client2 = V2.client
+          client3 = V3.upgradeClientV2ToV3 client2
       -- In practice we'll only have this case i.e. the latest version of the
       -- client
       3 ->
         runClient
-          V3.protocolVersion
+          (ProtocolVersion 3)
+          (ProtocolVersion (min maxV 3))
           ( \(ProtocolVersion v) -> case v of
               3 -> SomePeer V3.codec V3.client
-              _ -> error $ "Unknown version: " ++ show v
+              _ -> error $ "Unsupported version: " ++ show v
           )
-      _ -> error $ "Unknown version: " ++ versionStr
+      _ -> error $ "Unsupported version: " ++ versionStr
     _ ->
       putStrLn $
         unlines
           [ "Usage:",
-            "cabal run -- c[lient] protocolVersion",
-            "cabal run -- s[erver]"
+            "cabal run -- c[lient] clientProtocolVersion maxSupportedVersion",
+            "cabal run -- s[erver] serverVersion"
           ]
